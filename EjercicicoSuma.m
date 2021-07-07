9a5b0bfe99bec53b4ca522a8b4bcb19455d9bd76
MarisolRosaldo <82452696+MarisolRosaldo@users.noreply.github.com>
2021-07-07 13:00:10 -0500

    Update EjercicicoSuma.m

--- EjercicicoSuma.m
+++ EjercicicoSuma.m
@@ -3,15 +3,15 @@
 %Función: 3x+3, [6,9]
 
  a=6;
->> b=9;
->> n=100;
->> intX= ((b-a)/n);
->> area=0;
->> x=a;
->> for i=0:n-1;
+ b=9;
+n=100;
+intX= ((b-a)/n);
+area=0;
+x=a;
+for i=0:n-1;
 x+= intX;
 f= (3*x)+3;
 area= area +abs(intX*f);
 endfor
->> disp(['En el area= ', num2str(area), ' U.^2']);
+disp(['En el area= ', num2str(area), ' U.^2']);
 En el area= 76.635 U.^2
